21464b08a72934e7b1030f41011916b0051b32e0
rosamondkim <itzmems@naver.com>
2023-03-03 16:55:34 +0900

    remove: useWeather 커스텀훅 필요없는 내용 콘솔 제거

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,82 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import axios, { AxiosResponse } from 'axios';
-// import useCurrentLocation from 'hooks/useCurrentLocation';
-// import WeatherDescription from '../utils/WeatherDescription';
-// import WeatherImageMap from 'utils/WeatherImageMap';
-
-// //useWeather 커스텀 훅
-
-// interface WeatherInfo {
-//   logo: string;
-//   weather: string;
-//   temp: number;
-//   feels: number;
-//   tempMax: number;
-//   tempMin: number;
-//   humidity: number;
-//   umbrella: number;
-// }
-
-// const useWeather = (): WeatherInfo => {
-//   const { location, error } = useCurrentLocation();
-//   const OPEN_WEATHER_MAP_API = process.env.REACT_APP_OPEN_WEATHER_MAP_API;
-//   const [isLoading, setIsLoading] = useState<boolean>(true);
-
-//   const initWeatherInfo = {
-//     logo: '',
-//     weather: '',
-//     temp: 0,
-//     feels: 0,
-//     tempMax: 0,
-//     tempMin: 0,
-//     humidity: 0,
-//     windSpeed: 0,
-//     umbrella: 0,
-//   };
-
-//   const [weatherInfo, setWeatherInfo] = useState<WeatherInfo>(initWeatherInfo);
-
-//   useEffect(() => {
-//     if (error) {
-//       return;
-//     }
-//     if (!location) {
-//       return;
-//     }
-//     const getFetch = async () => {
-//       await axios
-//         .get(
-//           `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${OPEN_WEATHER_MAP_API}&units=metric`,
-//         )
-//         .then(
-//           (res: AxiosResponse) => updateWeatherInfo(res.data),
-//           (res: AxiosResponse) => setIsLoading(false),
-//         );
-//     };
-
-//     const updateWeatherInfo = (res: any) => {
-//       console.log(res);
-//       // 이 res 타입 수정해야함
-//       setIsLoading(false);
-//       setWeatherInfo({
-//         logo: WeatherImageMap[res.weather[0].id],
-//         temp: res.main.temp.toFixed(1),
-//         feels: res.main.feels_like.toFixed(1),
-//         tempMax: res.main.temp_max.toFixed(1),
-//         tempMin: res.main.temp_min.toFixed(1),
-//         humidity: res.main.humidity,
-//         weather: WeatherDescription[res.weather[0].id].title,
-//         umbrella: res.weather[0].id,
-//       });
-//     };
-//     getFetch();
-//   }, [location]);
-
-//   return { weatherInfo, isLoading };
-// };
-
-// export default useCurrentLocation;
-
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import useCurrentLocation from './useCurrentLocation';
@@ -120,7 +41,7 @@ function useWeather() {
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
         const response = await axios.get<WeatherData>(url);
         const data = response.data;
-        console.log(data);
+
         const weather: Weather = {
           feels: data.main.feels_like.toFixed(1),
           temperature: data.main.temp,
