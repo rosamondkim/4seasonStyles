7be9701ab71e482215dcf8eee5f8ee7dc8e2f167
rosamondkim <itzmems@naver.com>
2023-03-04 20:51:00 +0900

    style: splash 페이지 반응형 스타일 추가

--- src/pages/splashPage/SplashStyle.tsx
+++ src/pages/splashPage/SplashStyle.tsx
@@ -6,10 +6,22 @@ export const SplashScreen = styled.div`
 `;
 
 export const Logo = styled.img`
+  // 모바일
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 45.6em;
-  height: 29.4em;
+  width: 300px;
+  height: 193px;
+
+  @media (min-width: 390px) {
+    // 아이패드
+    width: 324px;
+    height: 209px;
+  }
+  @media (min-width: 834px) {
+    // 웹
+    width: 385px;
+    height: 248px;
+  }
 `;
