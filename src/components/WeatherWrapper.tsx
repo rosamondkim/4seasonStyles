6f163433ea20d6a5334c59eec54b34962d0fce81
rosamondkim <itzmems@naver.com>
2023-03-06 01:41:18 +0900

    fix: props 적용 안되는 현상 해결

--- src/components/WeatherWrapper.tsx
+++ src/components/WeatherWrapper.tsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 
 // props로 내려줘야할것 : width height flex background hover
 interface WrapperProps {
-  width?: string;
-  height?: string;
-  background?: string;
-  hover?: string;
+  widthVal?: string;
+  heightVal?: string;
+  backgroundImage?: string;
+  isHover?: string;
   flexDirection?: string;
   children?: ReactNode;
 }
 
 const WeatherWrapper = (props: WrapperProps) => {
-  return <WeatherWrapperDiv>{props.children}</WeatherWrapperDiv>;
+  return <WeatherWrapperDiv {...props}>{props.children}</WeatherWrapperDiv>;
 };
 
 export default WeatherWrapper;
@@ -22,27 +22,28 @@ export const WeatherWrapperDiv = styled.div<WrapperProps>`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex: 1;
   flex-direction: ${({ flexDirection }) => flexDirection ?? 'row'};
   width: 100%;
-  height: ${({ height }) => height ?? 'auto'};
+  height: ${({ heightVal }) => heightVal ?? 'auto'};
   background-color: var(--bg-color);
-  background-image: ${({ background }) => background ?? 'auto'};
+  background-image: ${({ backgroundImage }) => backgroundImage ?? 'auto'};
   box-shadow: var(--section-shadow);
   border-radius: var(--sec-border-radius);
   position: relative;
   z-index: 0;
 
   &:hover {
-    background: ${({ hover }) => hover ?? 'transparent'};
+    background: ${({ isHover }) => isHover ?? 'transparent'};
   }
   @media (min-width: 744px) {
     //패드
-    width: 347px;
-    height: auto;
+    padding: 1.7rem;
+    height: ${({ heightVal }) => heightVal ?? 'auto'};
   }
   @media (min-width: 1280px) {
     //웹
-    width: 467px;
-    height: auto;
+    padding: 5.5rem;
+    height: ${({ heightVal }) => heightVal ?? 'auto'};
   }
 `;
